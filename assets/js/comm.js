93c080fcbda7c23119bdba110fbea0802212b707
UDintheroom <dkaqktk38@gmail.com>
2024-03-05 18:01:47 +0900

    main intro add

---
+++ assets/js/comm.js
@@ -0,0 +1,9 @@
+// main intro
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/Z8x8fJKiotyuVcjr/scene.splinecode');
+
+
+//
